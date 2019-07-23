0490d378887e5c21b5dfcb918792e42a803b9b17
adefian <fianade8@gmail.com>
2019-07-23 14:59:03 +0700

    rev.02

---
+++ app/PemesananUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PemesananUser extends Model
+{
+    protected $fillable = [
+        'nomorSPA','nama', 'instansi', 'alamat','ntelp','contohygdianalisis',
+        'unsurygdianalisis','jml_contoh','bentuk','asal_contoh','merk'
+    ];
+
+
+    public function transaksi()
+    {
+        return $this->belongsTo('App\Transaksi');
+    }
+
+    
+}
